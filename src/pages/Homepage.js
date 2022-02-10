10d9363abc1cf25882b84683918f998ce95ebba6
Eric Moyer <ejmoyer3@comcast.net>
2022-02-09 20:51:34 -0500

    React and router initial set-up.

---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+//import { Link } from "react-router-dom";
+
+// <p><Link to="/2">Your desired link.</Link></p>
+ 
+export default function Homepage() {
+    return(
+        <article>
+            <h1>Welcome to E-Commerce Site!</h1>
+        </article>
+    );
+}
+
+
